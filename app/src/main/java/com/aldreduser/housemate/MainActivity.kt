0c15d7eb3005c0035bf3d4fc555201436d034f12
Ed Cres <crespoed07@gmail.com>
2021-01-19 18:08:39 -0500

    added ShoppingItem data class

--- app/src/main/java/com/aldreduser/housemate/MainActivity.kt
+++ app/src/main/java/com/aldreduser/housemate/MainActivity.kt
@@ -3,6 +3,10 @@ package com.aldreduser.housemate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// https://blog.mindorks.com/mvvm-architecture-android-tutorial-for-beginners-step-by-step-guide
+
+//todo: I'm gonna add the things for the chore item later, that way i can follow up with the example better
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/aldreduser/housemate/data/model/ShoppingItem.kt
@@ -0,0 +1,62 @@
+package com.aldreduser.housemate.data.model
+
+import com.google.gson.annotations.SerializedName
+
+// I wanna have the properties well organized bc it might make the recyclerview and storage easier.
+
+data class ShoppingItem (
+    // Necessary
+    @SerializedName("id")
+    val id: Int = 0,
+    @SerializedName("name")
+    val name: String = "",
+    @SerializedName("quantity") // Chores didn't have this property, but i put it here
+    val quantity: Double = 0.0,
+    @SerializedName("addedBy") // todo: idk the format for the value, i just did camelCase
+    val addedBy: String = "",
+    @SerializedName("purchased")
+    val purchased: Boolean = false,
+
+    // Not necessary and not used in Chores
+    @SerializedName("cost")
+    val cost: Double = 0.0,
+    @SerializedName("purchaseLocation")
+    val purchaseLocation: String = "",
+
+    // Not necessary and used in chores
+    @SerializedName("neededBy")
+    val neededBy: String = "",  //date
+    @SerializedName("volunteer")
+    val volunteer: String = "", //who's buying it
+    @SerializedName("priority")
+    val priority: Int
+)
+
+/*
+Shopping items properties:
+-id
+-item name
+-quantity
+-addedByWho
+-purchased?
+
+-cost
+-where it can be bought
+
+-needed by (this date)
+-who volunteers to buying it
+-priority 1-3 (compared to others shopping items)
+_________________________________________________________________________________
+
+Chores properties:
+-id
+-chore name
+-addedByWho
+-is it done?
+
+-difficultly 1-3
+
+-needed by (this date)
+-who volunteers to do it
+-priority 1-3 (compared to others shopping items)
+ */
