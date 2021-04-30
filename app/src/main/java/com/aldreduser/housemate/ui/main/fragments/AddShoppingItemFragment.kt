06989520761ba1784f5245e46e1357faf74240ae
Ed Cres <crespoed07@gmail.com>
2021-04-30 11:21:42 -0400

    Changes the another activity to a fragment to make it single activity app.

--- app/src/main/java/com/aldreduser/housemate/ui/main/fragments/AddShoppingItemFragment.kt
+++ app/src/main/java/com/aldreduser/housemate/ui/main/fragments/AddShoppingItemFragment.kt
@@ -1,14 +1,29 @@
-package com.aldreduser.housemate.ui.main.activities
+package com.aldreduser.housemate.ui.main.fragments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.aldreduser.housemate.R
-import kotlinx.android.synthetic.main.activity_add_shopping_item.*
+import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
 
-class AddShoppingItemActivity : AppCompatActivity() {
+class AddShoppingItemFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_shopping_item)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_shopping_item, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         setupAppBar()
         setupViewModel()
         fabOnClick()
