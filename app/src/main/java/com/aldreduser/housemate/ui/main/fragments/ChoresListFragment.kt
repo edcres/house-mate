3c0f13ddf119861b13a517b3b56e0cde16b86100
Ed Cres <crespoed07@gmail.com>
2021-05-01 10:56:40 -0400

    Set string resource in xml file, handled other warnings in xml files.

--- app/src/main/java/com/aldreduser/housemate/ui/main/fragments/ChoresListFragment.kt
+++ app/src/main/java/com/aldreduser/housemate/ui/main/fragments/ChoresListFragment.kt
@@ -16,38 +16,19 @@ import com.aldreduser.housemate.ui.main.viewmodels.lists.ChoresListViewModel
 import com.aldreduser.housemate.util.Status
 import kotlinx.android.synthetic.main.fragment_chores_list.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChoresListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChoresListFragment : Fragment() {
 
     private lateinit var choresListViewModel: ChoresListViewModel
     private lateinit var adapterRecyclerview: ChoresRecyclerviewListAdapter
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
 
         setupRecyclerView()
         setupObserver()
         setupViewModel()
 
         fabOnClick()
-
     }
 
     override fun onCreateView(
@@ -58,8 +39,9 @@ class ChoresListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_chores_list, container, false)
     }
 
-
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 
     private fun fabOnClick() {
         // todo: pass some data to that activity
@@ -84,7 +66,6 @@ class ChoresListFragment : Fragment() {
         )
         choresListRecyclerView.adapter = adapterRecyclerview
     }
-
 
     // Observer
     private fun setupObserver() {
@@ -119,31 +100,5 @@ class ChoresListFragment : Fragment() {
 //            this,
 //            ChoresListViewModelFactory(ApiHelper(ApiServiceImpl()))
 //        ).get(ChoresListViewModel::class.java)
-    }
-
-
-
-
-
-
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChoresListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChoresListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
