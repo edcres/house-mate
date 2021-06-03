c60d5f88b1e628f825cedcf8f4a4d344e74eab87
Ed Cres <crespoed07@gmail.com>
2021-06-02 22:43:44 -0400

    Updated the viewModel factory, it also now sends an application argument to the viewmodel.

--- app/src/main/java/com/aldreduser/housemate/ui/main/viewmodels/ListsViewModelFactory.kt
+++ app/src/main/java/com/aldreduser/housemate/ui/main/viewmodels/ListsViewModelFactory.kt
@@ -1,19 +1,20 @@
 package com.aldreduser.housemate.ui.main.viewmodels
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.aldreduser.housemate.data.ListsRepository
 
-class ListsViewModelFactory(private val repository: ListsRepository): ViewModelProvider.Factory {
-
-    lateinit var listsViewModel: ListsViewModel
+class ListsViewModelFactory(
+    private val repository: ListsRepository,
+    private val application: Application): ViewModelProvider.Factory {
 
     // returns a repository to the viewModel <OR> gives u an error if the model class doesn't show up
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ListsViewModel::class.java)) {
-//            listsViewModel = ListsViewModel(ListsRepository())
+            val listsViewModel = ListsViewModel(repository, application)
             return listsViewModel as T
-        }
-        throw IllegalArgumentException("Unknown class name")
+        } else { throw IllegalArgumentException("Unknown class name") }
     }
 }
