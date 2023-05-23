1abb63e297adc9c766be3d1cfb300905440a392f
Eduardo A. Crespo <crespoed07@gmail.com>
2023-05-23 07:13:46 -0400

    Changed file structure for home package.

--- hm-flutter-fldr/house_mate/lib/main.dart
+++ hm-flutter-fldr/house_mate/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'cubit/app_state.dart';
+import 'cubit/items_state.dart';
 
 // void main() {
 //   runApp(const MyApp());
