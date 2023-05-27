a10d629dce8d31b8d9b3350995bd3baabac89cd5
Eduardo A. Crespo <crespoed07@gmail.com>
2023-05-27 11:35:27 -0400

    Changed typo from HomeCubit to Lists cubit.

--- hm-flutter-fldr/house_mate/lib/pages/home/view/home_page.dart
+++ hm-flutter-fldr/house_mate/lib/pages/home/view/home_page.dart
@@ -14,3 +14,26 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+class _HomeTabButton extends StatelessWidget {
+  const _HomeTabButton({
+    required this.groupValue,
+    required this.value,
+    required this.icon,
+  });
+
+  final HomeTab groupValue;
+  final HomeTab value;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () => context.read<ListsCubit>().setTab(value),
+      iconSize: 32,
+      color:
+          groupValue != value ? null : Theme.of(context).colorScheme.secondary,
+      icon: icon,
+    );
+  }
+}
