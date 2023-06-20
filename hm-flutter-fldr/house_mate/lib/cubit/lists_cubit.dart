7c8be2c1ac89883dd8b9ce9f2beafc019ab951dc
Eduardo A. Crespo <crespoed07@gmail.com>
2023-06-20 02:13:38 -0400

    Made lists stae not constant

--- hm-flutter-fldr/house_mate/lib/cubit/lists_cubit.dart
+++ hm-flutter-fldr/house_mate/lib/cubit/lists_cubit.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'lists_state.dart';
 
 class ListsCubit extends Cubit<ListsState> {
-  ListsCubit() : super(const ListsState());
+  ListsCubit() : super(ListsState());
 
   // TODO: change this
   // void setTab(HomeTab tab) => emit(HomeState(tab: tab));
