709b72b2dc1d43d59842691c7003bbde8218793c
Eduardo A. Crespo <crespoed07@gmail.com>
2023-06-27 20:25:08 -0400

    Corrected overview to list.

--- hm-flutter-fldr/house_mate/lib/pages/items_list/bloc/items_list_state.dart
+++ hm-flutter-fldr/house_mate/lib/pages/items_list/bloc/items_list_state.dart
@@ -1,29 +1,29 @@
 part of 'items_list_bloc.dart';
 
-enum ItemsOverviewStatus { initial, loading, success, failure }
+enum ItemsListStatus { initial, loading, success, failure }
 
-class ItemsOverviewState extends Equatable {
-  const ItemsOverviewState({
-    this.status = ItemsOverviewStatus.initial,
+class ItemsListState extends Equatable {
+  const ItemsListState({
+    this.status = ItemsListStatus.initial,
     this.listItems = const [],
     this.filter = ItemsViewFilter.all,
     this.lastDeletedItem,
   });
 
-  final ItemsOverviewStatus status;
+  final ItemsListStatus status;
   final List<ListItem> listItems;
   final ItemsViewFilter filter;
   final ListItem? lastDeletedItem;
 
   Iterable<ListItem> get filteredItems => filter.applyAll(listItems);
 
-  ItemsOverviewState copyWith({
-    ItemsOverviewStatus Function()? status,
+  ItemsListState copyWith({
+    ItemsListStatus Function()? status,
     List<ListItem> Function()? items,
     ItemsViewFilter Function()? filter,
     ListItem? Function()? lastDeletedItem,
   }) {
-    return ItemsOverviewState(
+    return ItemsListState(
       status: status != null ? status() : this.status,
       listItems: items != null ? items() : this.listItems,
       filter: filter != null ? filter() : this.filter,
