f7051af347a8d5348487b39b33eb890b2d78b99b
Eduardo A. Crespo <crespoed07@gmail.com>
2023-07-05 02:40:47 -0400

    Populated edit item event file.

--- hm-flutter-fldr/house_mate/lib/pages/edit_item/bloc/edt_item_event.dart
+++ hm-flutter-fldr/house_mate/lib/pages/edit_item/bloc/edt_item_event.dart
@@ -1,1 +1,30 @@
+part of 'edit_item_bloc.dart';
 
+abstract class EditItemEvent extends Equatable {
+  const EditItemEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class EditItemTitleChanged extends EditItemEvent {
+  const EditItemTitleChanged(this.title);
+
+  final String title;
+
+  @override
+  List<Object> get props => [title];
+}
+
+class EditItemDescriptionChanged extends EditItemEvent {
+  const EditItemDescriptionChanged(this.description);
+
+  final String description;
+
+  @override
+  List<Object> get props => [description];
+}
+
+class EditItemSubmitted extends EditItemEvent {
+  const EditItemSubmitted();
+}
