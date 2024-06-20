fcb188012b39f1c58ef939830f552717939c3beb
Eduardo A. Crespo <crespoed07@gmail.com>
2024-06-20 17:33:49 -0400

    Created several constants for database fields and used them.

--- hm-flutter-fldr/house_mate/lib/data/firestore_api-service.dart
+++ hm-flutter-fldr/house_mate/lib/data/firestore_api-service.dart
@@ -2,39 +2,56 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:house_mate/Helper.dart';
 
 class FirestoreApiService {
-  // User ID
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final String GENERAL_COLLECTION = "todos";
+  final String GROUP_IDS_DOC = "Group IDs";
+  final String CLIENT_IDS_DOC = "Client IDs";
+  final String SHOPPING_LIST_DOC = "Shopping List";
+  final String SHOPPING_ITEMS_COLLECTION = "Shopping Items";
+  final String CHORES_LIST_DOC = "Chores List";
+  final String CHORE_ITEMS_COLLECTION = "Chore Items";
+
+  final String LAST_GROUP_ADDED_FIELD = "lastGroupAdded";
+  final String LAST_CLIENT_ADDED_FIELD = "lastClientAdded";
+  final String NAME_FIELD = "name";
+  final String QUANTITY_FIELD = "quantity";
+  final String ADDED_BY_FIELD = "addedBy";
+  final String COMPLETED_FIELD = "completed";
+  final String COST_FIELD = "cost";
+  final String PURCHASE_LOCATION_FIELD = "purchaseLocation";
+  final String NEEDED_BY_FIELD = "neededBy";
+  final String VOLUNTEER_FIELD = "volunteer";
+  final String PRIORITY_FIELD = "priority";
+  final String DIFFICULTY_FIELD = "difficulty";
+  final String NOTES_FIELD = "notes";
+  DocumentReference groupIDsDoc =
+      DocumentReference as DocumentReference<Object?>;
   Helper helper = Helper();
 
-  Future<String> createUserId() async {
-    final FirebaseFirestore firestore = FirebaseFirestore.instance;
-    final String groupId = '00000001aaaaa';
-    final DocumentSnapshot<Map<String, dynamic>> clientIdsDoc = await firestore
-        .collection('todos')
-        .doc('Group IDs')
-        .collection(groupId)
-        .doc('Client IDs')
-        .get();
+  FirestoreApiService() {
+    groupIDsDoc = firestore.collection(GENERAL_COLLECTION).doc(GROUP_IDS_DOC);
+  }
+
+  // User ID
+  Future<String> createUserId(String groupId) async {
+    final DocumentSnapshot<Map<String, dynamic>> clientIdsDoc =
+        await groupIDsDoc.collection(groupId).doc(CLIENT_IDS_DOC).get();
 
     String lastClientId =
-        clientIdsDoc.data()?['lastClientAdded'] ?? '00000001ffffe';
+        clientIdsDoc.data()?[LAST_CLIENT_ADDED_FIELD] ?? '00000001ffffe';
     String newClientId = helper.generateNewID(lastClientId);
 
-    await firestore
-        .collection('todos')
-        .doc('Group IDs')
+    await groupIDsDoc
         .collection(groupId)
-        .doc('Client IDs')
-        .set({'lastClientAdded': newClientId}, SetOptions(merge: true));
+        .doc(CLIENT_IDS_DOC)
+        .set({LAST_CLIENT_ADDED_FIELD: newClientId}, SetOptions(merge: true));
 
     return newClientId;
   }
 
   // Group ID
-  Future<String> createGroupId() async {
-    final FirebaseFirestore firestore = FirebaseFirestore.instance;
-    final QuerySnapshot<Map<String, dynamic>> groupsSnapshot = await firestore
-        .collection('todos')
-        .doc('Group IDs')
+  Future<String> createGroup() async {
+    final QuerySnapshot<Map<String, dynamic>> groupsSnapshot = await groupIDsDoc
         .collection('groups')
         .orderBy('id', descending: true)
         .limit(1)
@@ -46,24 +63,10 @@ class FirestoreApiService {
     String newGroupId = helper.generateNewID(lastGroupId);
 
     // Create lastClientAdded, ShoppingList, ChoresList
-    await firestore
-        .collection('todos')
-        .doc('Group IDs')
-        .collection(newGroupId)
-        .doc('Client IDs')
-        .set({'lastClientAdded': '00000001fffff'}, SetOptions(merge: true));
-    await firestore
-        .collection('todos')
-        .doc('Group IDs')
-        .collection(newGroupId)
-        .doc('Shopping List')
-        .set({});
-    await firestore
-        .collection('todos')
-        .doc('Group IDs')
-        .collection(newGroupId)
-        .doc('Chores List')
-        .set({});
+    await groupIDsDoc.collection(newGroupId).doc(CLIENT_IDS_DOC).set(
+        {LAST_CLIENT_ADDED_FIELD: '00000001fffff'}, SetOptions(merge: true));
+    await groupIDsDoc.collection(newGroupId).doc('Shopping List').set({});
+    await groupIDsDoc.collection(newGroupId).doc('Chores List').set({});
 
     return newGroupId;
   }
