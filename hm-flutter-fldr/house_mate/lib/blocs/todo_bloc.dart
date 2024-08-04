1ffc6cf441d9e3a59498b18ebeb2e4a9bdd89fd7
Eduardo A. Crespo <crespoed07@gmail.com>
2024-08-04 04:27:30 -0400

    Removed some unused imports.

--- hm-flutter-fldr/house_mate/lib/blocs/todo_bloc.dart
+++ hm-flutter-fldr/house_mate/lib/blocs/todo_bloc.dart
@@ -2,9 +2,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:house_mate/blocs/todo_event.dart';
 import 'package:house_mate/blocs/todo_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../data/models/todo_item.dart';
-import '../data/models/shopping_item.dart';
-import '../data/models/chore_item.dart';
 
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
   TodoBloc() : super(TodoState(items: [])) {
