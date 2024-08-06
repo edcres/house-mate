0787456f5185c1a1883d280ac2587f4363babb52
Eduardo A. Crespo <crespoed07@gmail.com>
2024-08-06 17:29:17 -0400

    Imported the event and state bloc into the tabs screen pt2.

--- hm-flutter-fldr/house_mate/lib/screens/tabs_screen.dart
+++ hm-flutter-fldr/house_mate/lib/screens/tabs_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:house_mate/blocs/todo_event.dart';
+import 'package:house_mate/blocs/todo_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../blocs/todo_bloc.dart';
 import 'items_screen.dart';
