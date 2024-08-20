cfe57945462714a1af167fa6ad4d228208ca0472
Eduardo A. Crespo <crespoed07@gmail.com>
2024-08-20 06:54:17 -0400

    Added more fields to the chore items class.

--- hm-flutter-fldr/house_mate/lib/data/models/chore_item.dart
+++ hm-flutter-fldr/house_mate/lib/data/models/chore_item.dart
@@ -3,8 +3,16 @@ import 'todo_item.dart';
 class ChoreItem extends TodoItem {
   ChoreItem({
     required String id,
-    required String task,
-    required bool isCompleted,
+    required String name,
+    required bool addedBy,
+    required bool completed,
+    // Not required
+    String neededBy = "",
+    String volunteer = "",
+    int priority = 3,
+    String notes = "",
+    // Only for Chores
+    int difficulty = 1,
   }) : super(
             id: id,
             name: task,
