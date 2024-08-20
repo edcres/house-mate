31f37ca4c7a2a256d358c3683b69b27709e7714e
Eduardo A. Crespo <crespoed07@gmail.com>
2024-08-20 04:28:19 -0400

    Removed a unused dialog file and unused imports.

--- hm-flutter-fldr/house_mate/lib/widgets/tabs_screen/tabs_dialogs.dart
+++ hm-flutter-fldr/house_mate/lib/widgets/tabs_screen/tabs_dialogs.dart
@@ -4,11 +4,6 @@ import 'package:house_mate/Helper.dart';
 import 'package:house_mate/blocs/todo_bloc.dart';
 import 'package:house_mate/blocs/todo_event.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:house_mate/blocs/todo_event.dart';
-import '../../blocs/todo_bloc.dart';
 import '../../data/models/todo_item.dart';
 
 class AddItemDialog extends StatefulWidget {
