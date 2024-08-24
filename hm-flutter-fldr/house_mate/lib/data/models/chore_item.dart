52b1ede93fdb7cdaf107f3f17cdefe37e09c789b
Eduardo A. Crespo <crespoed07@gmail.com>
2024-08-24 02:11:15 -0400

    Setting up to add a new item with the new fields.

--- hm-flutter-fldr/house_mate/lib/data/models/chore_item.dart
+++ hm-flutter-fldr/house_mate/lib/data/models/chore_item.dart
@@ -17,23 +17,20 @@ class ChoreItem extends TodoItem {
           itemType: ItemType.Chore,
         );
 
+  ChoreItem.fromJson(Map<String, dynamic> json)
+      : difficulty = (json[fieldDifficulty] as num?)?.toDouble() ?? 1.0,
+        super(
+          id: json[TodoItem.fieldId] as String,
+          name: json[TodoItem.fieldName] as String,
+          addedBy: json[TodoItem.fieldAddedBy] as String,
+          itemType: ItemType.Chore,
+        ) {
+    // Set the common fields using the parent class method
+    TodoItem.setCommonFields(this, json);
+  }
+
   @override
   List<Object?> get props => super.props..add(difficulty);
-
-  factory ChoreItem.fromJson(Map<String, dynamic> json) {
-    final commonFields = TodoItem.commonFromJson(json);
-    return ChoreItem(
-      id: commonFields[TodoItem.fieldId],
-      name: commonFields[TodoItem.fieldName],
-      addedBy: commonFields[TodoItem.fieldAddedBy],
-      // completed: commonFields[TodoItem.fieldCompleted],
-      // neededBy: commonFields[TodoItem.fieldNeededBy],
-      // volunteer: commonFields[TodoItem.fieldVolunteer],
-      // priority: commonFields[TodoItem.fieldPriority],
-      // notes: commonFields[TodoItem.fieldNotes],
-      difficulty: (json[fieldDifficulty] as num?)?.toDouble() ?? 1.0,
-    );
-  }
 
   @override
   Map<String, dynamic> toJson() {
@@ -44,3 +41,49 @@ class ChoreItem extends TodoItem {
     return json;
   }
 }
+
+
+// class ChoreItem extends TodoItem {
+//   static const String fieldDifficulty = 'difficulty';
+
+//   final double difficulty;
+
+//   ChoreItem({
+//     required String id,
+//     required String name,
+//     required String addedBy,
+//     this.difficulty = 1.0,
+//   }) : super(
+//           id: id,
+//           name: name,
+//           addedBy: addedBy,
+//           itemType: ItemType.Chore,
+//         );
+
+//   @override
+//   List<Object?> get props => super.props..add(difficulty);
+
+//   factory ChoreItem.fromJson(Map<String, dynamic> json) {
+//     final commonFields = TodoItem.commonFromJson(json);
+//     return ChoreItem(
+//       id: commonFields[TodoItem.fieldId],
+//       name: commonFields[TodoItem.fieldName],
+//       addedBy: commonFields[TodoItem.fieldAddedBy],
+//       // completed: commonFields[TodoItem.fieldCompleted],
+//       // neededBy: commonFields[TodoItem.fieldNeededBy],
+//       // volunteer: commonFields[TodoItem.fieldVolunteer],
+//       // priority: commonFields[TodoItem.fieldPriority],
+//       // notes: commonFields[TodoItem.fieldNotes],
+//       difficulty: (json[fieldDifficulty] as num?)?.toDouble() ?? 1.0,
+//     );
+//   }
+
+//   @override
+//   Map<String, dynamic> toJson() {
+//     final json = super.toJson();
+//     json.addAll({
+//       fieldDifficulty: difficulty,
+//     });
+//     return json;
+//   }
+// }
